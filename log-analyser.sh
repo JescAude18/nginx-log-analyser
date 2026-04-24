2788055b96787b7ef47de2c92bf1c72d956b0ecb
JescAude18 <jessicamoussougan@gmail.com>
2026-04-24 09:58:15 +0100

    feat :sparkles: : added a linux command to display the top 5 IP adresses with the most requests

--- log-analyser.sh
+++ log-analyser.sh
@@ -10,5 +10,6 @@
 log_file="nginx.log"
 
 echo -e "Welcome to the Nginx log analyser !\n"
-logs=$(cat ${log_file})
-echo "Content = ${logs}"
+echo -e "Top 5 IP adresses with the most requests :\n"
+TOP_5_IP=$(cat ${log_file} | awk '{print $1}' | sort | uniq -c | sort -k1 -n -r | head -5 | awk '{print $2 " - " $1 " requests"}')
+echo "${TOP_5_IP}"
