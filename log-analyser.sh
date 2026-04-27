c558eda7ffc90bb8e96f8f0a33133edd6d34c4ea
JescAude18 <jessicamoussougan@gmail.com>
2026-04-27 11:22:36 +0100

    feat :sparkles: : added linux commands combination to display top 5 response status codes

--- log-analyser.sh
+++ log-analyser.sh
@@ -16,3 +16,6 @@ echo -e "${TOP_5_IP}\n"
 echo "Top 5 most requested paths :"
 TOP_5_PH=$(cat ${log_file} | awk -F '"' '{print $2}' | awk '{print $2}' | sort | uniq -c | sort -k1 -n -r | head -5 | awk '{print $2 " - " $1 " requests"}')
 echo -e "${TOP_5_PH}\n"
+echo "Top 5 response status codes :"
+TOP_5_ST=$(cat nginx.log | awk -F '"' '{print $3}'| awk '{print $1}' | sort | uniq -c | sort -nrk1 | head -5 | awk '{print $2 " - " $1 " requests" }')
+echo -e "${TOP_5_ST}\n"
