d44f296f16315e8676d8652cd49f2ed9ddac1f68
JescAude18 <jessicamoussougan@gmail.com>
2026-04-27 12:35:41 +0100

    style :lipstick: : added some colors to the output and a welcome message to make the script more 'pretty'

--- log-analyser.sh
+++ log-analyser.sh
@@ -9,24 +9,28 @@
 
 log_file="nginx.log"
 
-echo -e "Welcome to the Nginx log analyser !\n"
+echo -e "\033[35m=============================== NGINX LOG ANALYSER ! ======================================="
+echo -e "Welcome ! This nginx log analyser. It displays statistics on the top 5 IP addresses, most "
+echo -e "requested paths, top response status codes, and top user agents of a nginx logs file."
+echo -e "Thanks for trying it ♥️\033[0m"
+echo -e "\033[35m============================================================================================\n"
 
 # Top 5 IP adresses with the most requests
-echo "Top 5 IP adresses with the most requests :"
+echo -e "\033[42mTop 5 IP adresses with the most requests :\033[0m"
 TOP_5_IP=$(cat ${log_file} | awk '{print $1}' | sort | uniq -c | sort -k1 -n -r | head -5 | awk '{print $2 " - " $1 " requests"}')
 echo -e "${TOP_5_IP}\n"
 
 # Top 5 most requested paths
-echo "Top 5 most requested paths :"
+echo -e "\033[43mTop 5 most requested paths :\033[0m"
 TOP_5_PH=$(cat ${log_file} | awk -F '"' '{print $2}' | awk '{print $2}' | sort | uniq -c | sort -k1 -n -r | head -5 | awk '{print $2 " - " $1 " requests"}')
 echo -e "${TOP_5_PH}\n"
 
 # Top 5 response status codes
-echo "Top 5 response status codes :"
+echo -e "\033[44mTop 5 response status codes :\033[0m"
 TOP_5_ST=$(cat nginx.log | awk -F '"' '{print $3}'| awk '{print $1}' | sort | uniq -c | sort -nrk1 | head -5 | awk '{print $2 " - " $1 " requests" }')
 echo -e "${TOP_5_ST}\n"
 
 # Top 5 user agents
-echo "Top 5 user agents :"
+echo -e "\033[41mTop 5 user agents :\033[0m"
 TOP_5_UA=$(cat ${log_file} | awk -F '"' '{print $6}' | sort | uniq -c | sort -nrk1 | head -5 | awk '{print $2 " - " $1 " requests" }')
-echo -e "${TOP_5_UA} :\n"
+echo "${TOP_5_UA}"
